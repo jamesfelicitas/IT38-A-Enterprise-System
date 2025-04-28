142bba0273f25e036d95ce2b5e754500a5b4aa82
jamesfelicitas <jamesivanfelicitas@gmail.com>
2025-04-28 22:50:17 +0800

    Initialize project with PHP template and external CSS

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Medicare Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+</body>
+</html>
