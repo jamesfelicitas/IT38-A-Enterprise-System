b692dda451ce4ba70df3dc10c039ab3bcd21bac7
jamesfelicitas <jamesivanfelicitas@gmail.com>
2025-04-28 22:52:14 +0800

    Add header and sidebar navigation layout

--- index.php
+++ index.php
@@ -9,6 +9,29 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div class="header">
+    <div>
+        <h2 class="logo">MEDICARE</h2>
+    </div>
+    <div>
+        <nav>
+            <a href="#">Home</a> |
+            <a href="#">Services</a> |
+            <a href="#">Appointment</a> |
+            <a href="#">About us</a> |
+            <a href="#">Contact</a>
+        </nav>
+    </div>
+</div>
 
+<div class="sidebar">
+    <a href="#">Dashboard</a>
+    <a href="#">Schedule</a>
+    <a href="#">Message</a>
+    <a href="#">Activity</a>
+    <a href="#">Security</a>
+    <a href="#">Settings</a>
+    <a href="#">Log out</a>
+</div>
 </body>
 </html>
