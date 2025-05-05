eee0c0c34000fc6a25ee96ef14519741c462e34d
jamesfelicitas <jamesivanfelicitas@gmail.com>
2025-05-05 15:05:12 +0800

    added function.php

---
+++ index.php
@@ -0,0 +1,60 @@
+
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Medicare Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="header">
+    <div>
+        <h2 class="logo">MEDICARE</h2>
+    </div>
+    <div>
+        <nav>
+            <a href="#">Home</a> |
+            <a href="#">Services</a> |
+            <a href="#">Appointment</a> |
+            <a href="#">About us</a> |
+            <a href="#">Contact</a>
+        </nav>
+    </div>
+</div>
+
+<div class="sidebar">
+    <a href="#">Dashboard</a>
+    <a href="#">Schedule</a>
+    <a href="#">Message</a>
+    <a href="#">Activity</a>
+    <a href="#">Security</a>
+    <a href="#">Settings</a>
+    <a href="#">Log out</a>
+</div>
+
+<div class="main">
+    <h3>Good Morning!</h3>
+    <div class="last-chat">
+        <h4>Last Chat</h4>
+        <input type="text" placeholder="Search Doctor...">
+        <ul>
+            <li>Doctor 1</li>
+            <li>Doctor 2</li>
+            <li>Doctor 3</li>
+        </ul>
+    </div>
+    <div class="patient-history">
+        <div class="chart">In-Patient</div>
+        <div class="chart">Schedule</div>
+        <div class="chart">Out-Patient</div>
+        <div class="chart">Consultancy</div>
+    </div>
+
+
+</div>
+
+</body>
+</html>
